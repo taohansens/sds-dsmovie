45fc46e39bea3b0853cd7c8b020147b7340fdbad
taohansens <taohansens@gmail.com>
2022-05-10 22:38:42 -0300

    MovieCard

---
+++ frontend/src/components/MovieScore/index.tsx
@@ -0,0 +1,14 @@
+import MovieStars from 'components/MovieStars';
+import './styles.css';
+
+export default function MovieScore() {
+    const score = 4;
+    const count = 21;
+    return (
+        <div className="dsmovie-score-container">
+            <p className="dsmovie-score-value">{score > 0 ? score.toFixed(1) : '-'}</p>
+            <MovieStars />
+            <p className="dsmovie-score-count">{count} avaliações</p>
+        </div>
+    );
+}
